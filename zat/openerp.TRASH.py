a76e9fb11bb7a033b8f04fddbf1bad9a513f3db6
Javier Revilla <jrevilla.pcm@gmail.com>
2020-01-12 10:33:23 -0500

    Clean

--- zat/openerp.TRASH.py
+++
@@ -1,5 +0,0 @@
-
-"""
-			- Total Productive Maintenance (TPM) focuses on Maintenance, rather than on Production. 
-			- The bulk of the work lies not in Manufactoring, but in Maintenance (or its avoidance).
-"""
