0dfdf6f068049ecfc6de46977ab1073fee79eb61
Javier Revilla <jrevilla.pcm@gmail.com>
2020-10-11 19:40:32 -0500

    Cleaning.

--- models/management/__init__.py
+++ models/management/__init__.py
@@ -3,44 +3,29 @@
 # Root
 from . import management
 
-
 # Family
 from . import family
 from . import subfamily
 
-
-
 # Productivity
 from productivity import *
-
 
 # Doctor Daily
 from doctor_daily import *
 
-
-
-# Order
-#from order import *
-
-
-
 # Doctor
 from doctor import *
-
-
-
 
 # Family
 from family import *
 
-
 # Patient
 from patient import *
-
 
 from . import management_order_line
 from . import family_line
 from . import subfamily_line
 
-
 #from . import day_doctor_line  # Dep ?
+# Order
+#from order import *
